842ea2a12edb6a755b4977f7cba3790fb9848d65
Marina1232 <marina.saiko.18@gmail.com>
2022-08-15 17:56:09 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.previousAssumption = 0;
+    this.result = null;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    const middleValue = Math.ceil((this.max + this.min) / 2);
+    this.previousAssumption = middleValue;
+    return middleValue;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.previousAssumption;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.previousAssumption;
+  }
 }
 
 module.exports = GuessingGame;
